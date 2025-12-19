b971b807d78136a36cefbe95b01b2b0f7f16da3d
lucasoliveira-dev01 <lucasoliveira09030@gmail.com>
2025-12-19 18:17:55 -0300

    adicionado client de email

---
+++ src/main/java/com/javanauta/bff_agendador_tarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,14 @@
+package com.javanauta.bff_agendador_tarefas.infrastructure.client;
+
+
+import com.javanauta.bff_agendador_tarefas.business.dto.out.TarefasDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+
+public interface EmailClient {
+
+  void enviarEmail(@RequestBody TarefasDTOResponse dto);
+
+    }
